c869c908a025a2f91a23b6fa00d926904af9250d
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-06-29 16:55:09 +0530

    added instructions to code

--- create and move files to its own folder.py
+++ create and move files to its own folder.py
@@ -8,3 +8,18 @@ for filename in os.listdir(directory):
         path = filename.split('.')[0]
         os.mkdir(path)
         shutil.move(filename, path + '/' + filename)
+        
+
+'''     
+This is a Python script to make folder for selected files and then move those files to corresponding folders with same name as file name.
+
+Instructions to use
+1. Place this python script in folder containing files
+2. Specify the file extension that you want to select(default is .pdf)
+3. Now run the script in powershell or cmd(shift + right click and select open in windows terminal)
+4. The files with given extension will now be placed inside corresponding folders with same name as file name
+
+WARNING
+In editions of Windows before Windows 10 version 1607, the maximum length for a path is MAX_PATH, which is defined as 260 characters.
+In later versions of Windows, changing a registry key or using the Group Policy tool is required to remove the limit.
+'''
